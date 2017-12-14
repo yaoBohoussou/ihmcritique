600ef65b4d1bd6851b8c5b6499dfd1f26d6466a5
Bohoussou Yao yann vianney <ybohouss@localhost.localdomain>
2017-12-14 13:30:01 +0100

    first commit

---
+++ BE_SysCr/src/be_syscr/BoutonDialStates.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package be_syscr;
+
+/**
+ *
+ * @author ybohouss
+ */
+public enum BoutonDialStates {
+    
+    WXON,
+    TST,
+    WXA,
+    STDBY,
+    OFF;
+    
+}
---
+++ feuTricolore/src/feutricolore/EtatFeu.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package feutricolore;
+
+/**
+ *
+ * @author ybohouss
+ */
+public enum EtatFeu {
+    Eteint, 
+    Rouge,
+    Orange,
+    Vert,
+    ErreurOrangeOn,
+    ErreurOrangeOff;
+}
